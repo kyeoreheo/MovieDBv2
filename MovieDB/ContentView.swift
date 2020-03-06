90e210a2eb43a6f4ea6348285eaafd2795abfab1
kyeoreheo <91kyoheo@gmail.com>
2020-03-06 10:52:54 -0500

    Initial Commit

---
+++ MovieDB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieDB
+//
+//  Created by Kyo on 3/6/20.
+//  Copyright © 2020 Kyo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
